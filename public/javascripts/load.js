4efb043fea621edc36bc0efa96ac8f8e8139225d
md-mostafa <md.mostafa.akash@gmail.com>
2021-01-19 15:22:14 +0600

    fix switch

--- public/javascripts/load.js
+++ public/javascripts/load.js
@@ -1,8 +1,54 @@
 $(document).ready(function() {
     var data = [];
-    $('input[type="checkbox"]').change(function() {  
-        data.push(this.value);
-        //console.log(data);
+    $('input[type="checkbox"]').change(function() {
+        $.each($("input[type='checkbox']:checked"), function() {
+            if(!data.includes($(this).val())){
+                data.push($(this).val());
+            }         
+        });
+        $.each($('input[type="checkbox"]:not(:checked)'), function() {
+            if(data.includes($(this).val())){
+                var index = data.indexOf($(this).val());
+                data.splice(index, 1);
+            }
+        });
+        
+        console.log(data);
+        $.ajax({
+            url: '/load',
+            type: 'POST',
+            cache: false,
+            data: {data: data.length === 0 ? '' : data},
+            success: function(data){
+              //alert('Success!')
+           }
+           , error: function(jqXHR, textStatus, err){
+               alert('text status '+textStatus+', err '+err)
+           }
+        })
+    })
+
+});
+
+
+/*
+
+
+$(document).ready(function() {
+    var data = [];
+    $('input[type="checkbox"]').change(function() {
+        if($('input[type="checkbox"]').is(':checked')){
+            if(!data.includes(this.value)){
+                data.push(this.value);
+            }
+        }else if(!$('input[type="checkbox"]').is(":checked")) {
+            if(data.includes(this.value)){
+                var index = data.indexOf(this.value);
+                data.splice(index, 1);
+            }
+        }
+        
+        console.log(data);
         $.ajax({
             url: '/load',
             type: 'POST',
@@ -19,16 +65,6 @@ $(document).ready(function() {
 
 })
 
-/*
-$(document).ready(function() {
-    $('input[type=checkbox]').onClick(function() {
-        var favorite = [];
-        $.each($("input[name='switch']:checked"), function(){
-            favorite.push($(this).val());
-        });
-        alert("My favourite sports are: " + favorite.join(", "));
-    })
-    
-})
 
 */
+
--- routes/load.js
+++ routes/load.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../controllers/db');
 var ip = require('ip');
+var fs = require('fs');
 
 router.get('/', function(req, res, next) {
     res.render('load');
@@ -9,10 +10,19 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', function(req, res, next){
-    console.log("L-1", req.body.data[0]);
-    console.log("L-2",req.body.data[1]);
-    console.log("L-3",req.body.data[2]);
-    //console.log(req.body.data[].[0]);
+    //console.log("L-1", req.body.data[0]);
+    //console.log("L-2",req.body.data[1]);
+    //console.log("L-3",req.body.data[2]);
+    var data = '';
+    if(req.body.data[0])
+        data = data + 1;
+    if(req.body.data[1])
+        data = data + 2;
+    if(req.body.data[2])
+        data = data + 3;
+    //console.log('data', data);
+   
+   
     res.redirect('/load');
 })
 
